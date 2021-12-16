85640366b03beb597baa6f3669effbb03406e677
Vincent <vquatrevieux@b2pweb.com>
2021-12-16 11:45:07 +0100

    Missing test file

---
+++ tests/_files/Php8Syntax.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace _files;
+
+class Php8Syntax
+{
+    public function withUnion(A|B $b)
+    {
+
+    }
+
+    public function withBuiltinType(string $foo, callable $bar)
+    {
+
+    }
+}
+
+class A
+{
+
+}
+
+class B
+{
+
+}
